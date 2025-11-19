e042a6271ff55779ab84082ff4ff0f31e91ae26f
JPMaheux <jpmaheux@users.noreply.github.com>
2025-11-19 14:16:15 -0500

    MCRM-536 Fork of react-native-scrollable-tab-view to use new viewpager package (#1)
    update package.json
    use new react-native-pager-view package
    renamed prop ref as its a reserved keyword
    added typing
    removed examples

---
+++ index.d.ts
@@ -0,0 +1,139 @@
+import * as React from 'react';
+import { ScrollViewProps, TextStyle, ViewStyle } from 'react-native';
+
+export interface ChangeTabProperties {
+    // currentPage
+    i: number;
+    // currentPage object
+    component: any;
+    // previousPage
+    from?: number;
+}
+
+export interface ScrollableTabViewChildProps {
+    tabLabel: string;
+}
+
+export interface ScrollableTabViewProps {
+    /**
+     * Position of the tab bar
+     */
+    tabBarPosition?: "top" | "bottom" | "overlayTop" | "overlayBottom";
+
+    /**
+     * Initial page to show
+     */
+    initialPage?: number;
+
+    /**
+     * Current page
+     */
+    page?: number;
+
+    /**
+     * Callback when tab changes
+     */
+    onChangeTab?: (tab: ChangeTabProperties) => void;
+
+    /**
+     * Callback when scrolling
+     */
+    onScroll?: (x: number) => void;
+
+    /**
+     * Custom tab bar renderer
+     */
+    renderTabBar?: () => React.ReactElement | false;
+
+    /**
+     * Style for tab bar underline
+     */
+    tabBarUnderlineStyle?: ViewStyle;
+
+    /**
+     * Background color of tab bar
+     */
+    tabBarBackgroundColor?: string;
+
+    /**
+     * Active text color
+     */
+    tabBarActiveTextColor?: string;
+
+    /**
+     * Inactive text color
+     */
+    tabBarInactiveTextColor?: string;
+
+    /**
+     * Text style for tab bar
+     */
+    tabBarTextStyle?: TextStyle;
+
+    /**
+     * Style for the main container
+     */
+    style?: ViewStyle;
+
+    /**
+     * Props passed to the ScrollView/ViewPager
+     */
+    contentProps?: ScrollViewProps;
+
+    /**
+     * Change tab without animation
+     */
+    scrollWithoutAnimation?: boolean;
+
+    /**
+     * Lock scroll
+     */
+    locked?: boolean;
+
+    /**
+     * Pre-render sibling pages
+     */
+    prerenderingSiblingsNumber?: number;
+
+    /**
+     * Child components (tabs)
+     */
+    children?: React.ReactNode;
+}
+
+export interface DefaultTabBarProps {
+    goToPage?: (pageNumber: number) => void;
+    activeTab?: number;
+    tabs?: Array<string | React.ReactElement>;
+    backgroundColor?: string;
+    activeTextColor?: string;
+    inactiveTextColor?: string;
+    textStyle?: TextStyle;
+    tabStyle?: ViewStyle;
+    renderTab?: (
+        name: string | React.ReactElement,
+        page: number,
+        isTabActive: boolean,
+        onPressHandler: () => void
+    ) => React.ReactElement;
+    underlineStyle?: ViewStyle;
+    style?: ViewStyle;
+}
+
+export interface ScrollableTabBarProps extends DefaultTabBarProps {
+    scrollOffset?: number;
+    tabsContainerStyle?: ViewStyle;
+}
+
+declare class DefaultTabBar extends React.Component<DefaultTabBarProps> {}
+declare class ScrollableTabBar extends React.Component<ScrollableTabBarProps> {}
+
+declare class ScrollableTabView extends React.Component<ScrollableTabViewProps> {
+    static DefaultTabBar: typeof DefaultTabBar;
+    static ScrollableTabBar: typeof ScrollableTabBar;
+
+    goToPage(pageNumber: number): void;
+}
+
+export default ScrollableTabView;
+export { DefaultTabBar, ScrollableTabBar };
--- index.js
+++ index.js
@@ -13,16 +13,15 @@ const {
   InteractionManager,
 } = ReactNative;
 
-const ViewPagerAndroid = require('@react-native-community/viewpager');
 const TimerMixin = require('react-timer-mixin');
-const ViewPager = require('@react-native-community/viewpager');
+import PagerView from 'react-native-pager-view';
 
 const SceneComponent = require('./SceneComponent');
 const DefaultTabBar = require('./DefaultTabBar');
 const ScrollableTabBar = require('./ScrollableTabBar');
 
 const AnimatedViewPagerAndroid = Platform.OS === 'android' ?
-  Animated.createAnimatedComponent(ViewPager) :
+  Animated.createAnimatedComponent(PagerView) :
   undefined;
 
 const ScrollableTabView = createReactClass({
@@ -317,7 +316,7 @@ const ScrollableTabView = createReactClass({
   _onChangeTab(prevPage, currentPage) {
     this.props.onChangeTab({
       i: currentPage,
-      ref: this._children()[currentPage],
+      component: this._children()[currentPage],
       from: prevPage,
     });
   },
